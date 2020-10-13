5f37e5a46562448b20571cb49b1a2e66037a96c9
Brian Zhang <brian.pcchang@gmail.com>
2020-10-13 23:44:31 +0800

    Added post model

---
+++ server.js
@@ -0,0 +1,8 @@
+console.log('Hello!');
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.end('My first response');
+});
+
+server.listen(process.env.PORT || 3000);

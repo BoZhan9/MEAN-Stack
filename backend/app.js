8bb0446ccf7844fd34a1ab91736fae179a9e5e91
Brian Zhang <brian.pcchang@gmail.com>
2020-10-18 20:15:13 +0800

    Fetched initial posts

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
+const { Message } = require('@angular/compiler/src/i18n/i18n_ast');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'abcdef',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'abcdefghi',
+      title: 'Second server-side post',
+      content: 'This is coming from the server!'
+    }
 
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+  ];
+  return res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;

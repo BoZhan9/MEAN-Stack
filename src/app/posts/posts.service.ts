481ebb9094fe6051e98df3ecb8af68a81d9e518f
Brian Zhang <brian.pcchang@gmail.com>
2020-10-22 01:38:14 +0800

    Update posts.service.ts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,7 +41,7 @@ export class PostsService {
       .post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post)
       .subscribe(responseData => {
         const id = responseData.postId;
-        post.id = id;
+        post.id = id; //update the id create by post
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });

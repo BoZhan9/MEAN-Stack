c18edde7697998a8e877b48d0215311a210a6826
Brian Zhang <brian.pcchang@gmail.com>
2020-10-26 23:47:42 +0800

    No red line error but save post function broken

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
 import { Post } from '../post.model';
 
@@ -17,6 +17,8 @@ export class PostCreateComponent implements OnInit{
   private postId: string;
   post: Post; //should be public
   isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
 
   constructor(
     public postsService: PostsService,
@@ -24,6 +26,13 @@ export class PostCreateComponent implements OnInit{
   ) {}
 
   ngOnInit() {//parent map is an observable
+    this.form = new FormGroup({
+      title: new FormControl(null, { //beginning form state
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, { validators: [Validators.required] })
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has("postId")) {
         this.mode = "edit";
@@ -31,7 +40,15 @@ export class PostCreateComponent implements OnInit{
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content};
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+          this.form.setValue({ //override the values for form controls to initialize the post
+            title: this.post.title,
+            content: this.post.content
+          });
         });
       } else {
         this.mode = "create";
@@ -40,20 +57,21 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
+    this.isLoading = true;
     if (this.mode === "create") {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postsService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
 
